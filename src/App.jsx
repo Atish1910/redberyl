734d1b4a2f288afc62f818dc3f4d4d5acd454547
Ashish Vinayak Kamble <atishkamble398@gmail.com>
2025-07-04 10:42:33 +0530

    01

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <section>
+        <div className="container">
+          <div className="row">
+            <h1> hii i am App COmponent</h1>
+          </div>
+        </div>
+      </section>
+    </>
+  );
+}
+
+export default App;
